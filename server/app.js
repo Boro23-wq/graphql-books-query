8e036331328e668b7d8c6880e1f618f146967c20
Boro23 <404slimboysavage@gmail.com>
2021-01-04 19:22:39 +0530

    added-mongodb-and-resolvers

--- server/app.js
+++ server/app.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// DB connection
+const dbURI =
+  'mongodb+srv://23Boro:Zeej2VwElxRFljn0@cluster0.eutdg.gcp.mongodb.net/gql-books?retryWrites=true&w=majority';
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((result) => app.listen(3000))
+  .then(console.log('MongoDB Connected!'))
+  .catch((err) => console.log(err));
+
 // bind express with graphql
-app.use('/graphql', graphqlHTTP({
+app.use(
+  '/graphql',
+  graphqlHTTP({
     schema,
-    graphiql: true
-}));
+    graphiql: true,
+  })
+);
 
 app.listen(4000, () => {
-    console.log('now listening for requests on port 4000');
+  console.log('Now listening for requests on port 4000!');
 });
