31ce0936b5b7d4f48dbace1b484fc9b93563386c
Boro23 <404slimboysavage@gmail.com>
2021-01-05 20:37:40 +0530

    added-client

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+// import components
+
+import BookList from './components/BookList';
+
+function App() {
+  return (
+    <div id='main'>
+      <h1>Reading List</h1>
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
